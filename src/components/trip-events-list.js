e5e0a50f4c71fc294c68d5e583467b97d7dc3fa2
Ivan <ivanprivalov@mail.ru>
2020-04-10 01:59:35 +0300

    Diving the code into modules

---
+++ src/components/trip-events-list.js
@@ -0,0 +1,13 @@
+const createTripEventsList = () => {
+  return (
+    `<ul class="trip-days">
+      <li class="trip-days__item  day">
+        <div class="day__info"></div>
+        <ul class="trip-events__list">
+        </ul>
+      </li>
+    </ul>`
+  );
+};
+
+export {createTripEventsList};
