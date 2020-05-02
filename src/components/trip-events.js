70abbe383879955043f16838566fbe4c2420fff2
Ivan <ivanprivalov@mail.ru>
2020-05-02 04:46:11 +0300

    Creating data structures and implementing components rendering functions

--- src/components/trip-events.js
+++ src/components/trip-events.js
@@ -22,7 +22,7 @@ const createTripEventsTemplate = (event) => {
     `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
         </div>
         <h3 class="event__title">${getEventTitle(event)}</h3>
 
