6d6d6a3020e709c60901c9cb8b9bf5826e8c0467
Ivan <ivanprivalov@mail.ru>
2020-05-20 20:13:37 +0300

    Implementing databinding

--- src/controllers/trip-events-list.js
+++ src/controllers/trip-events-list.js
@@ -1,55 +1,18 @@
 import {generateEvents} from '../mock/trip-events.js';
-import {render, replace, remove} from '../utils/render.js';
+import {render, remove} from '../utils/render.js';
 import {getEventsPerDay} from '../utils/common.js';
 
-import TripEventComponent from '../components/trip-event.js';
-import EventFormComponent from '../components/event-form.js';
 import NoEventsComponent from '../components/no-events.js';
 import SortingComponent, {SortType} from '../components/sorting.js';
 import TripEventsListComponent, {SORTED_ARRRAY_KEY} from '../components/trip-events-list.js';
+
+import TripEventController from './trip-event.js';
 
 const sortedEventsKey = SORTED_ARRRAY_KEY;
 
 const EVENTS_COUNT = 15;
 
 const tripEvents = generateEvents(EVENTS_COUNT);
-const eventsPerDay = getEventsPerDay(tripEvents);
-
-
-const renderEvent = (dayElement, event) => {
-
-  const replaceEventToEdit = () => {
-    replace(eventFormComponent, tripEventComponent);
-  };
-
-  const replaceEditToEvent = () => {
-    replace(tripEventComponent, eventFormComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToEvent();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const tripEventComponent = new TripEventComponent(event);
-  tripEventComponent.setEditButtonClickHandler(() => {
-    replaceEventToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  const eventFormComponent = new EventFormComponent(event);
-  eventFormComponent.setSubmitHandler((evt) => {
-    evt.preventDefault();
-    replaceEditToEvent();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(dayElement, tripEventComponent, `beforeend`);
-};
 
 const getSortedEvents = (events, sortType) => {
   let sortedEvents = {
@@ -63,7 +26,7 @@ const getSortedEvents = (events, sortType) => {
 
   switch (sortType) {
     case SortType.EVENT:
-      sortedEvents = eventsPerDay;
+      sortedEvents = getEventsPerDay(events);
       break;
     case SortType.TIME:
       sortedEvents.sorted = eventsToSort
@@ -80,21 +43,35 @@ const getSortedEvents = (events, sortType) => {
 class TripController {
   constructor(container) {
     this._container = container;
-    this._structuredEvents = eventsPerDay;
+
+    this._events = [];
+    this._structuredEvents = {};
+
+    this._eventControllers = [];
 
     this._noEventsComponent = new NoEventsComponent();
     this._sortingComponent = new SortingComponent();
-    this._tripEventsListComponent = new TripEventsListComponent(this._structuredEvents);
+    this._tripEventsListComponent = null;
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onSortTypeChange = this._onSortTypeChange.bind(this);
+    this._onViewChange = this._onViewChange.bind(this);
+
+    this._sortingComponent.setSortTypeChangeHandler(this._onSortTypeChange);
   }
 
-  render(structuredEvents = this._structuredEvents) {
+  _renderEvents(events, onDataChange, onViewChange) {
 
-    if (Object.keys(structuredEvents).length === 0) {
-      render(this._container, this._noEventsComponent, `beforeend`);
-      return;
-    }
+    const renderEvent = (event, container) => {
+      const tripEventController = new TripEventController(container, onDataChange, onViewChange);
+      tripEventController.render(event);
 
-    render(this._container.querySelector(`h2`), this._sortingComponent, `afterend`);
+      return tripEventController;
+    };
+
+    const eventControllers = [];
+
+    this._tripEventsListComponent = new TripEventsListComponent(this._structuredEvents);
 
     render(this._container, this._tripEventsListComponent, `beforeend`);
 
@@ -103,25 +80,64 @@ class TripController {
     tripDayElements.forEach((element) => {
       const eventsList = element.querySelector(`.trip-events__list`);
 
-      if (structuredEvents.hasOwnProperty(sortedEventsKey)) {
-        structuredEvents[sortedEventsKey].forEach((event) => {
-          renderEvent(eventsList, event);
+      if (events.hasOwnProperty(sortedEventsKey)) {
+        events[sortedEventsKey].forEach((event) => {
+          eventControllers.push(renderEvent(event, eventsList));
         });
       } else {
         const dayNumber = element.querySelector(`.day__counter`).textContent;
 
-        structuredEvents[dayNumber].forEach((event) => {
-          renderEvent(eventsList, event);
+        events[dayNumber].forEach((event) => {
+          eventControllers.push(renderEvent(event, eventsList));
         });
       }
     });
 
-    this._sortingComponent.setSortTypeChangeHandler(() => {
-      remove(this._tripEventsListComponent);
-      this._structuredEvents = getSortedEvents(tripEvents, this._sortingComponent.getSortType());
-      this._tripEventsListComponent.setEvents(this._structuredEvents);
-      this.render(this._structuredEvents);
+    return eventControllers;
+  }
+
+  _updateEvents(events) {
+    this._events = events;
+    this._structuredEvents = getEventsPerDay(this._events);
+  }
+
+  render(events = tripEvents) {
+    this._updateEvents(events);
+
+    const isEventsAbsent = Object.keys(this._structuredEvents).length === 0;
+
+    if (isEventsAbsent) {
+      render(this._container, this._noEventsComponent, `beforeend`);
+      return;
+    }
+
+    render(this._container.querySelector(`h2`), this._sortingComponent, `afterend`);
+
+    this._eventControllers = this._renderEvents(this._structuredEvents, this._onDataChange, this._onViewChange);
+  }
+
+  _onViewChange() {
+    this._eventControllers.forEach((it) => {
+      it.setDefaultView();
     });
+  }
+
+  _onSortTypeChange() {
+    remove(this._tripEventsListComponent);
+    this._structuredEvents = getSortedEvents(this._events, this._sortingComponent.getSortType());
+    this._eventControllers = this._renderEvents(this._structuredEvents, this._onDataChange, this._onViewChange);
+  }
+
+  _onDataChange(eventController, oldData, newData) {
+    const index = this._events.findIndex((it) => it === oldData);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._updateEvents([].concat(this._events.slice(0, index), newData, this._events.slice(index + 1)));
+
+    eventController.render(this._events[index]);
   }
 }
 
